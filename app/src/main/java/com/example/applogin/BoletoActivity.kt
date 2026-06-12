11c87b4b1fdd10313c3f74e03fd067b3b7f02204
Filipe <filipejosecamargo2014@gmail.com>
2026-06-12 09:40:51 -0300

    initial commit

---
+++ app/src/main/java/com/example/applogin/BoletoActivity.kt
@@ -0,0 +1,37 @@
+package com.example.applogin
+
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class BoletoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_boleto)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val toolbarBoleto = findViewById<Toolbar>(R.id.toolbarBoleto)
+        setSupportActionBar(toolbarBoleto)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                finish() // Volta para a tela anterior
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
