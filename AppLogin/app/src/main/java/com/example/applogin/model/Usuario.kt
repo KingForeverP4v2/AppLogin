dc08eb9cff7c21a71e213ffa89a4c5b16909cdbc
KingForeverP4v2 <ericacnakanishi@gmail.com>
2026-06-12 10:36:42 -0300

    Delete AppLogin directory

--- AppLogin/app/src/main/java/com/example/applogin/model/Usuario.kt
+++
@@ -1,9 +0,0 @@
-package com.example.applogin.model
-
-data class Usuario(
-    var nome : String,
-    var cpf : String,
-    var email : String,
-    var telefone : String,
-    var senha : String,
-)
