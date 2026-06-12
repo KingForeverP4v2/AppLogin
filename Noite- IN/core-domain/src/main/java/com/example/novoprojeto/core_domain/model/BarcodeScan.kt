15c70e2f9a665145d89a93d3ad9c17c06e761959
KingForeverP4v2 <ericacnakanishi@gmail.com>
2026-06-12 10:36:57 -0300

    Delete Noite- IN directory

--- Noite- IN/core-domain/src/main/java/com/example/novoprojeto/core_domain/model/BarcodeScan.kt
+++
@@ -1,7 +0,0 @@
-package com.example.novoprojeto.core_domain.model
-import java.time.Instant
-
-data class BarcodeScan(
-    val value: BarcodeValue,
-    val scannedAt: Instant
-)
--- Noite- IN/core-domain/src/main/java/com/example/novoprojeto/core_domain/usecase/SaveBarcodeUseCase.kt
+++
@@ -1,3 +0,0 @@
-package com.example.novoprojeto.core_domain.usecase
-
-
